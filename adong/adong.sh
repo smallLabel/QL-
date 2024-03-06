5ef345af53c705a777f938d4fb04e591017f7225
smallLabel <1007243939@qq.com>
2024-03-06 14:57:47 +0800

    Update adong.sh

--- adong/adong.sh
+++ adong/adong.sh
@@ -288,7 +288,7 @@ rm -f adbot/adbot
 
 file=env.properties
 if [ ! -f "$file" ]; then
-  wget -O env.properties https://ghproxy.com/https://raw.githubusercontent.com/shidahuilang/QL-/main/adong/env.properties
+  wget -O env.properties https://mirror.ghproxy.com/https://raw.githubusercontent.com/shidahuilang/QL-/main/adong/env.properties
 else
   echo "env.properties已存在"
 fi
